263236ba76e8efd22674d3189638c3f4474be70d
Robert Corrigan <rjcorrig@users.noreply.github.com>
2016-11-27 15:22:06 -0500

    Fix for Express mount point stripping
    Uses req.originalUrl as first choice for generating the redirection url, as Express strips the mount point from req.url during routing, losing path information. If req.originalUrl is not available (i.e. we're not using Express), fall back to req.url, which should have the original path.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ module.exports = function (opts) {
       return;
     }
 
-    var url = req.originalUrl;
+    var url = (req.originalUrl || req.url);
     var host = req.headers.host || '';
 		if (!/:\d+/.test(host) && 443 !== opts.port) {
 			// we are using standard port 80, but we aren't using standard port 443
